6f9273fa1cdc9802396a0964e6b6d22230e4c775
Whiteford, Mr B <b.whiteford@tga.org.uk>
2019-08-26 10:13:35 +0100

    add MS sds pull

---
+++ mssds.ps1
@@ -0,0 +1,82 @@
+#requies -modules pwsh-module-api-wrapper, ad-user-provisioning
+if (!(test-path ./mscsv)) {
+    new-item -itemtype directory -path ./mscsv 
+}
+
+$pConn = @{
+    URL   = $env:GOR_API_URL
+    Token = $env:GOR_API_TOKEN
+}
+
+# school csv
+$orgs = Get-ApiContent @pConn -Endpoint "orgs" -all
+$orgs.orgs |
+select @{n = 'DfE Number'; e = { $_.sourcedid } },
+Name | 
+export-csv ./mscsv/schools.csv
+
+# teacher csv
+$usersTeachers = Get-ApiContent @pConn -Endpoint "users?filter=role='teacher' AND status='Y'" -all
+$usersTeachers.Users |
+Where-Object username -ne $null | 
+Select @{n = 'ID'; e = { $_.SourcedId } },
+@{n = 'Secondary Email'; e = { $_.email } },
+@{n = 'School DfE Number'; e = { $_.orgs.SourcedId -join ',' } },
+username | 
+export-csv ./mscsv/teacher.csv
+
+# student csv
+$userPupil = Get-ApiContent @pConn -Endpoint "users?filter=role='student' AND status='Y'" -all
+$userPupil.Users |
+Select *, @{ n = 'YearIndex'; e = { ConvertFrom-K12 -Year $_.grades -ToIndex } } | 
+Where-Object YearIndex -ge 4 | 
+Select @{n = 'ID'; e = { $_.SourcedId } },
+Username,
+@{n = 'Secondary Email'; e = { $_.email } },
+@{n = 'School DfE Number'; e = { $_.orgs.SourcedId -join ',' } },
+@{n = 'FirstName'; e = { $_.givenName } },
+@{n = 'LastName'; e = { $_.familyName } } | 
+export-csv ./mscsv/student.csv
+
+# section csv
+$AS = Get-ApiContent @pConn -Endpoint "academicSessions" -all
+
+$classes = Get-ApiContent @pConn -Endpoint "classes" -all
+$classes.classes | select @{n = 'ID'; e = { $_.sourcedId } },
+@{n = 'Section Name'; e = { $_.title } },
+@{n = 'School DfE Number'; e = { $_.school.sourcedId -join ',' } },
+@{n = 'term StartDate'; e = { 
+    $sd = $_.terms.sourcedId 
+    ($as.AcademicSessions | ? sourcedid -eq $sd | select startDate).startDate 
+    } },  
+@{n = 'term EndDate'; e = {
+    $ed = $_.terms.sourcedId
+    ($as.AcademicSessions | ? sourcedid -eq $ed | select enddate).EndDate
+    } } | 
+export-csv ./mscsv/sections.csv
+
+# Student enrollment
+$senrollments = Get-ApiContent @pConn -Endpoint "enrollments?filter=role='student'" -all
+$senrollments.Enrollments |
+select @{n = 'Section ID'; e = { $_.class.sourcedId } },
+@{n = 'ID'; e = { 
+    $id = $_.user.sourcedId
+    if ($id.count -gt 1) {
+        $id[0]
+    }
+    else {
+        $id
+    }  
+  } } |
+export-csv ./mscsv/studentenrollment.csv
+
+# teacher roster
+$tenrollments = Get-ApiContent @pConn -Endpoint "enrollments?filter=role='teacher'" -all
+$tenrollments.Enrollments |
+select @{n = 'Section ID'; e = { $_.class.sourcedId } },
+@{n = 'ID'; e = { 
+    $_.user.sourcedId
+ } } |
+? ID -ne $null |
+export-csv ./mscsv/teacherroster.csv
+
