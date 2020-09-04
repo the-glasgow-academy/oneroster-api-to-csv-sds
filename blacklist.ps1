8ee16d16ca83eb23dd362e8dc187e66a26a79720
fffnite <bradley.whiteford@live.co.uk>
2020-09-04 16:35:12 +0100

    Split blacklist to seperate file, add time-checking to token

---
+++ blacklist.ps1
@@ -0,0 +1,47 @@
+function blacklistClasses {
+    
+    param(
+        [Parameter(Mandatory,ValueFromPipeline,Position=0)]
+        [object]$i
+    )
+
+    Process {
+        $i | Where-Object {
+            ($_.title -like "*Tutorial*") -or
+            ($_.title -like "*Sports*") -or
+            ($_.title -like "*Instrumental*") -or
+            ($_.title -like "*Topic*") -or
+            ($_.title -like "*Swimming*") -or
+            ($_.title -like "*Supervised*") -or
+            ($_.title -like "*Kindergarten*") -or
+            ($_.title -like "*MN*") -or
+            ($_.title -like "*Music P4 Group*") -or
+            ($_.title -like "Support for Learning P2") -or 
+            ($_.title -like "Support for Learning LT") -or
+            ($_.title -like "Learing Support*") -or 
+            ($_.YearIndex -ge 0 -and $_.YearIndex -le 2) -or
+            ($_.title -like "Dept Meeting*") -or
+            ($_.title -like "BUSY:*") 
+        }
+    }
+}
+
+function blacklistUsers {
+
+    param(
+        [Parameter(Mandatory,ValueFromPipeline,Position=0)]
+        [object]$i
+    )
+
+    Process {
+        $i | Where-Object {
+            ($_.role -eq 'aide') -or 
+            ($_.status -eq 'tobedeleted') -or
+            ($_.email -eq 'NULL') -or
+            ($_.email -like '') -or
+            ($_.familyName -like '*ACCOUNT*') -or
+            ($_.YearIndex -ge 0 -and $_.YearIndex -le 2)
+        }
+    }
+}
+
